d281d55527c3816390906e8ea8c3004903e208a3
Minea Ervio <minea.ervio@aalto.fi>
2020-12-16 21:57:43 +0200

    3.22.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const express = require('express')
 const app = express()
 require('dotenv').config()
@@ -11,39 +12,39 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
-morgan.token("body", (req, res) => {
-  return JSON.stringify(req.body);
-});
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body)
+})
 app.use(
-  morgan(":method :url :status :response-time ms - :res[content-length] :body")
-);
-  
+  morgan(':method :url :status :response-time ms - :res[content-length] :body')
+)
+
 app.get('/api/persons', (req, res) => {
   Contact.find({}).then(persons => {
     res.json(persons)
   })
 })
- 
+
 app.get('/info', async (req, res) => {
   const date = new Date
   const dateText = date.toString()
   const count = await Contact.find({})
   res.send(
-  `<div>Phonebook has info for ${count.length} people</div>
+    `<div>Phonebook has info for ${count.length} people</div>
   <div>${dateText}</div>`
   )
 })
-  
+
 app.get('/api/persons/:id', (request, response, next) => {
   Contact.findById(request.params.id)
-  .then(person => {
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -53,15 +54,15 @@ app.delete('/api/persons/:id', (request, response, next) => {
     })
     .catch(error => next(error))
 })
-  
+
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
-  
+
   const person = new Contact({
     name: body.name,
     number: body.number
   })
-    
+
   person.save()
     .then(savedContact => {
       response.json(savedContact)
@@ -91,20 +92,20 @@ app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
-  
+
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   }
-  
+
   next(error)
 }
-  
+
 app.use(errorHandler)
 
 const port = process.env.PORT
 
 app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
-    })
+  console.log(`Server running on port ${port}`)
+})
